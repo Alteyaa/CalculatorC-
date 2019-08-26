54509b80cc4d58a4027b68845204e3c63a3b23d4
MacBookPro <alteyaa>
2019-08-26 20:24:32 +0600

    first commit

---
+++ Calculators/EmptyClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+namespace Calculators
+{
+    public class EmptyClass
+    {
+
+
+        private double firstnumber;
+        private double secondnumber;
+        private double result;
+        private char operation;
+
+
+        public EmptyClass(double first, double second, char oop)
+        {
+            firstnumber      = first;
+            secondnumber     = second;
+            operation        = oop;
+            Switching();
+
+        }
+
+        private void Switching()
+        {
+            switch (operation)
+            {
+                case '+':
+                    result = firstnumber + secondnumber;
+
+                   Console.WriteLine("The sum of {0} and {1} is {2}",firstnumber,secondnumber,result);
+                    startover();
+                    break;
+
+                case '-':
+                    result = firstnumber - secondnumber;
+                    Console.WriteLine("The diference {0} and {1} is {2}", firstnumber, secondnumber,result);
+                    startover();
+                    break;
+                case '*':
+                    result = firstnumber * secondnumber;
+                    Console.WriteLine("The product of {0} and {1} is {2}", firstnumber, secondnumber,result);
+                    startover();
+                    break;
+                case '/':
+                    result = firstnumber / secondnumber;
+                    Console.WriteLine("The division of {0} and {1} is {2}", firstnumber, secondnumber,result);
+                    startover();
+                    break;
+                case '%':
+                    result = firstnumber % secondnumber;
+                    Console.WriteLine("The procent of {0} and {1} is {2}", firstnumber, secondnumber,result);
+                    startover();
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.Please enter valid operation in next time");
+                    End();
+                    break;
+              
+            }
+        }
+        private void startover()
+        {
+            Console.WriteLine("You want to do more? If \'yes\' then press enter (Y)." +
+                "Else enter any key");
+            string YesorNo = Console.ReadLine();
+            bool mybool = (YesorNo == "y" || YesorNo == "Y");
+            if(mybool == true)
+            {
+                repeat();
+
+            }
+            else
+            {
+                End();
+            }
+           
+        }
+        private void repeat()
+        {
+            firstnumber = result;
+            Console.WriteLine("Input Second Number");
+            secondnumber= double.Parse(Console.ReadLine());
+
+            Console.WriteLine("choose an operation :  (+, -, *, /, %, ^)");
+            operation = char.Parse(Console.ReadLine());
+
+        }
+
+        private void End()
+        {
+            Console.WriteLine("Final Result is {0}", result);
+            Console.Write("Press any key to Exit the calculator");
+            Console.ReadKey();
+
+        }
+        }
+    }
+
---
+++ Calculators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculators
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("\t\t\t Concole Calculator\r");
+            Console.WriteLine("\t\t\t-------------------------------\r");
+
+
+            Console.WriteLine("\t\t\tInput first number\n");
+            double firstnumber = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t\t\t Enter econd number");
+            double secondnumber = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\t\t\tChose an : (+, -, *, /, %, ^)\n");
+            char myopp = char.Parse(Console.ReadLine());
+
+            EmptyClass User = new EmptyClass(firstnumber, secondnumber, myopp);
+
+        }
+    }
+}
